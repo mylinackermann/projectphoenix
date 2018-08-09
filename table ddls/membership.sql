f66d8375c4aec7a69f5278a87df0a3e7b7813358
Mylin Ackermann <mylin.ackermann@slalom.com>
2018-08-09 10:19:21 -0500

    more ddls

---
+++ table ddls/membership.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [dbo].[membership]
+(
+[OPERA_BATCH_CD] varchar(100),
+[MEMBERSHIP_ID] int,
+[MEMBERSHIP_TYPE] varchar(100),
+[MEMBERSHIP_LEVEL] varchar(100),
+[JOINED_DATE] varchar(100),
+[EXPIRATION_DATE] varchar(100),
+[POINT_INDICATOR] varchar(100),
+[PRIMARY_AIRLINE_PARTNER] varchar(100),
+[CURRENT_POINTS] varchar(100),
+[MEMBER_INDICATOR] varchar(100),
+[MEMBER_SUBTYPE] varchar(100),
+[INACTIVE_DATE] varchar(100),
+[ENROLLMENT_CODE] varchar(100),
+[MEMBERSHIP_STATUS] varchar(100),
+[TRACK_DATA] varchar(100),
+[EARNING_PREFERENCE] varchar(100),
+[INSERT_DATE] date,
+[UPDATE_DATE] date,
+);
