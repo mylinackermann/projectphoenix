d0aaef4ed5628341a7282201909ea7098c88ade0
Mylin Ackermann <mylin.ackermann@slalom.com>
2018-08-09 09:38:12 -0500

    table ddls added

---
+++ table ddls/allotment_changes.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[allotment_changes]
+(
+[ALLOTMENT_CHANGE_ID] int,
+[RESORT] varchar(100),
+[ALLOTMENT_HEADER_ID] int,
+[STAY_DATE] date,
+[ROOMNIGHTS] varchar(100),
+[ROOM_REVENUE] varchar(100),
+[BOOKING_STATUS] varchar(100),
+[INSERT_DATE] date,
+[UPDATE_DATE] date
+);
